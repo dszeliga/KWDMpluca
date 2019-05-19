b3c81936217cebecb17dc5ad6ee5bb7deb1a6404
chmurapaulina <paulchm347@student.polsl.pl>
2019-05-19 21:51:46 +0200

    Poprawione wyświetlanie. Nowa baza: https://wiki.cancerimagingarchive.net/display/Public/Lung+CT+Segmentation+Challenge+2017

--- KWDMpluca/KWDMpluca/Helpers/BitmapHelper.cs
+++ KWDMpluca/KWDMpluca/Helpers/BitmapHelper.cs
@@ -37,8 +37,7 @@ namespace KWDMpluca.Helpers
                     for (int c = 0; c < columns; c++)
                     {
                         int j = ((int)(l * rows * columns) + (int)(r * columns) + (int)c) * 2;
-                        Y[r, c] = ((float)bufor[j] *50)*(float)bufor[j + 1];
-                        //Y[r, c] = (float)bufor[j] *(float)bufor[j + 1]+1000;                        
+                        Y[r, c] = (double)bufor[j + 1] * 256 + (double)bufor[j];
                         if (Y[r, c] > m)
                         {
                             m = Y[r, c];
